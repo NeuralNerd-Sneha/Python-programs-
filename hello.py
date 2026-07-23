f941c349177c2a62d7a59fd961e894542233d021
NeuralNerd-Sneha <pawarsneha0612@gmail.com>
2026-07-24 01:17:28 +0530

    added python programs

---
+++ hello.py
@@ -0,0 +1,15 @@
+# My First Python Program
+
+print("Hello, I am Sneha")
+print("I am learning Python")
+print("I am a NeuralNerd!")
+
+# Simple calculation
+a = 10
+b = 20
+sum = a + b
+print(f"Sum of {a} and {b} is {sum}")
+
+# Your name
+name = "Sneha"
+print(f"Welcome {name}!")
